6a65fa97903fcd98a9e5ddefd0dfdd02c57d0213
shunsuke227ono <8144677452@mail.ecc.u-tokyo.ac.jp>
2016-04-26 14:35:44 +0900

    fix assignment

--- tutorial/assignments.py
+++ tutorial/assignments.py
@@ -1,5 +1,11 @@
 # coding: utf8
 
+# NOTE: This is the output when this file gets executed.
+# $ python assignments.py
+# => [Assinment1] Leap years between 1990 and 2200: [1992, 1996, 2000, 2004, 2008, 2012, 2016, 2020, 2024, 2028, 2032, 2036, 2040, 2044, 2048, 2052, 2056, 2060, 2064, 2068, 2072, 2076, 2080, 2084, 2088, 2092, 2096, 2104, 2108, 2112, 2116, 2120, 2124, 2128, 2132, 2136, 2140, 2144, 2148, 2152, 2156, 2160, 2164, 2168, 2172, 2176, 2180, 2184, 2188, 2192, 2196]
+# => [Assignment3] Average in total: 67.200000
+# => [Assignment3] Average in leap years: 65.333333
+
 data = [
     { 'name': '田中花子', 'year': 1980, 'score': 58 },
     { 'name': '鈴木一郎', 'year': 2000, 'score': 76 },
@@ -8,7 +14,7 @@ data = [
     { 'name': '石井あや', 'year': 1978, 'score': 71 }
 ]
 
-# NOTE: This method is answer for assinment 2.
+# NOTE: This method is answer for assignment2.
 def is_leap_year(year):
     if year % 400 == 0:
         return True
@@ -16,7 +22,7 @@ def is_leap_year(year):
         return False
     return year % 4 == 0
 
-# NOTE: This method is answer for assinment 1.
+# NOTE: This method is answer for assignment1.
 def leap_years(start_year, end_year):
     leap_years = []
     for y in range(start_year, end_year + 1):
@@ -27,14 +33,14 @@ def leap_years(start_year, end_year):
 def avg(x):
     return 1.0 * sum(x) / len(x)
 
-# NOTE: This method is answer for assinment 3
+# NOTE: This method is answer for assginment3.
 def average(data):
     scores = []
     for datum in data:
         scores.append(datum['score'])
     return avg(scores)
 
-# NOTE: This method is answer for assinment 3
+# NOTE: This method is answer for assginment3.
 def average_of_leap_years(data):
     scores = []
     for datum in data:
@@ -42,16 +48,16 @@ def average_of_leap_years(data):
             scores.append(datum['score'])
     return avg(scores)
 
-def assinment1():
+def assignment1():
     years = leap_years(1990, 2200)
-    print years
+    print "[Assinment1] Leap years between 1990 and 2200: %s" % years
 
-def assinment3():
+def assignment3():
     ave = average(data)
     leap_ave = average_of_leap_years(data)
-    print ave
-    print leap_ave
+    print "[Assignment3] Average in total: %f" % ave
+    print "[Assignment3] Average in leap years: %f" % leap_ave
 
 if __name__ == '__main__':
-    assinment1()
-    assinment3()
+    assignment1()
+    assignment3()
