8e136648f7d3e63937c29499633effd240c88f73
shunsuke227ono <8144677452@mail.ecc.u-tokyo.ac.jp>
2016-05-12 17:46:07 +0900

    add sraping script for commedian data

---
+++ collect_data/commedian_wiki.rb
@@ -0,0 +1,24 @@
+require 'open-uri'
+require 'nokogiri'
+
+url = 'https://ja.wikipedia.org/wiki/%E5%90%89%E6%9C%AC%E8%88%88%E6%A5%AD%E6%89%80%E5%B1%9E%E3%82%BF%E3%83%AC%E3%83%B3%E3%83%88%E4%B8%80%E8%A6%A7'
+
+charset = nil
+html = open(url) do |f|
+  charset = f.charset
+  f.read
+end
+
+# htmlをパース(解析)してオブジェクトを生成
+doc = Nokogiri::HTML.parse(html, nil, charset)
+
+taget = doc.css("#mw-content-text ul li")
+
+yoshimoto_commedians = []
+doc.css('#mw-content-text').css('ul').each do |ul|
+  ul.css('li').each do |li|
+    yoshimoto_commedians << li.css('a').inner_text
+  end
+end
+
+p yoshimoto_commedians
